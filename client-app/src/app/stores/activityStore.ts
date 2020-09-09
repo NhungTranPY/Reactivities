dae0030dbde2e4378658290637a7a340637fc329
NhungTranPY <nhungtranpy91@gmail.com>
2020-09-09 15:09:12 +0700

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -17,9 +17,20 @@ class ActivityStore {
     //use computed when we already have existing data, and how the data base
     @computed get activitiesByDate() {
         //return this.activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date)) tuong tu:
-        return Array.from(this.activityRegistry.values()).sort(
+        //console.log(this.groupActivitiesByDate(Array.from(this.activityRegistry.values())))
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
+    }
+
+    //bai 111: grouping activities by date
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivities = activities.sort(
             (a, b) => Date.parse(a.date) - Date.parse(b.date)
         )
+        return Object.entries(sortedActivities.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0]
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity]
+            return activities
+        }, {} as {[key: string]: IActivity[]}))
     }
 
     @action loadActivities = async () => { //async bai 84
@@ -35,6 +46,7 @@ class ActivityStore {
                 });
                 this.loadingInitial = false
             })
+            console.log(this.groupActivitiesByDate(activities))
         } catch (error) {
             runInAction('load activities error', () => {
                 this.loadingInitial = false
